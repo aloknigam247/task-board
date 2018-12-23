57e779d10815f3d7f779259f920616e1a086a133
alok.monotype <aloknigam247@gmail.com>
2018-12-23 15:20:13 +0530

    fixes for project Improve UI
    moved "delete, move up and move down" to the action bar
    fixes #17
    fixes #36

--- app/src/main/java/com/app/taskboard/MainActivity.java
+++ app/src/main/java/com/app/taskboard/MainActivity.java
@@ -3,6 +3,9 @@ package com.app.taskboard;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
@@ -17,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
         mRun = new Run(R.layout.activity_main, this, this);
         mRun.startUp();
         mTaskList = mRun.getTaskList();
@@ -56,6 +61,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actions, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(Actions.itemClicked) {
+            switch (item.getItemId()) {
+                case R.id.action_delete: {
+                    mTaskList.remove(Actions.itemSelected);
+                    break;
+                }
+                case R.id.action_move_up: {
+                    Actions.itemSelected = mTaskList.moveUp(Actions.itemSelected);
+                    break;
+                }
+                case R.id.action_move_down: {
+                    Actions.itemSelected = mTaskList.moveDown(Actions.itemSelected);
+                    break;
+                }
+                default:
+                    return super.onOptionsItemSelected(item);
+            }
+        }
+        return true;
+    }
+
+    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         UserPermission.onRequestResult(requestCode, permissions, grantResults);
@@ -64,19 +98,13 @@ public class MainActivity extends AppCompatActivity {
     void registerActions() {
         ActivityRegistration register = new ActivityRegistration();
         register.registerResource(R.layout.activity_main, R.id.addTask, new Actions(), mTaskList, Actions.ADD_TASK);
-        register.registerResource(R.layout.activity_main, R.id.deleteTask, new Actions(), mTaskList, Actions.DELETE_TASK);
         register.registerResource(R.layout.activity_main, R.id.taskDisplay, new Actions(), Actions.ONCLICK);
-        register.registerResource(R.layout.activity_main, R.id.moveUp, new Actions(), mTaskList, Actions.UP);
-        register.registerResource(R.layout.activity_main, R.id.moveDown, new Actions(), mTaskList, Actions.DOWN);
     }
 }
 
 class Actions implements ActivityRegistration.RegisterClick {
     final static int ADD_TASK    = 0;
-    final static int DELETE_TASK = 1;
     final static int ONCLICK     = 2;
-    final static int UP          = 3;
-    final static int DOWN        = 4;
     static boolean itemClicked = false;
     static int itemSelected;
 
@@ -87,22 +115,6 @@ class Actions implements ActivityRegistration.RegisterClick {
                 EditText newTask = (EditText)Run.findViewByActivity(R.layout.activity_main, R.id.newTask);
                 mTaskList.add(new Task(newTask.getText().toString()));
                 newTask.setText("");
-                break;
-            }
-            case DELETE_TASK: {
-                mTaskList.remove(itemSelected);
-                break;
-            }
-            case UP: {
-                if(itemClicked) {
-                    itemSelected = mTaskList.moveUp(itemSelected);
-                }
-                break;
-            }
-            case DOWN: {
-                if(itemClicked) {
-                    itemSelected = mTaskList.moveDown(itemSelected);
-                }
                 break;
             }
         }
